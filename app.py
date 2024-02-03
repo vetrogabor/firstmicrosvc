8f14f6cdd8449260b043c2e8b9d4e3c876b2a0b7
Sándor Kardos <kardos@ngms.hu>
2024-02-03 08:35:13 +0100

    Added code for returning client and server IPs.

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import socket
 
 app = Flask(__name__)
 
 @app.route("/add", methods=["POST"])
 def add_numbers():
+
     numbers = request.get_json()
     first_number = int(numbers["firstNumber"])
     second_number = int(numbers["secondNumber"])
 
-    total = first_number + second_number
+    # Retrieve the server's own IP address
+    try:
+        server_ip = socket.gethostbyname(socket.gethostname())
+    except:
+        server_ip = 'unknown'
 
-    return {"total": total}
+    # Create a JSON response containing both IPs
+    return jsonify({
+        "client_ip": request.remote_addr,
+        "server_ip": server_ip,
+        "total": first_number + second_number
+    })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=5000)
 
